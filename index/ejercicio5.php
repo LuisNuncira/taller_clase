4c3a2e5d2a3546cb5c3708e2e44bc359dc228852
DubanSanchez04 <dubsanchez@uniboyaca.edu.co>
2025-03-26 20:46:23 -0500

    dbs

---
+++ index/ejercicio5.php
@@ -0,0 +1,7 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $numero = intval($_POST["numero"]);
+    $binario = decbin($numero);
+    echo "<p>El número binario es: <strong>\$binario</strong></p>";
+}
+?>
